15a1ee4d8c0837ce352dc797236145ac9c5d09fc
Ashraful Islam <ashrafulislam.dev1@gmail.com>
2024-10-29 00:38:01 +0600

    Added Link to the header component using react router dom

--- src/main.jsx
+++ src/main.jsx
@@ -1,21 +1,26 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./index.css";
 import App from "./App.jsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./pages/Home/Home.jsx";
+import About from "./pages/About/About.jsx";
+import Contact from "./pages/Contact/Contact.jsx";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>This is home page</div>,
-  },
-  {
-    path: "/about",
-    element: <div>About page</div>,
-  },
-  {
-    path: "/contact",
-    element: <div>Contact Page</div>,
+    element: <Home />,
+    children: [
+      {
+        path: "/about",
+        element: <About />,
+      },
+      {
+        path: "/contact",
+        element: <Contact />,
+      },
+    ],
   },
 ]);
 
---
+++ src/pages/Contact/Contact.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Contact = () => {
+  return (
+    <div>
+      <h2>Contact Us</h2>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore optio
+        praesentium veniam. Totam doloribus temporibus pariatur dolore labore
+        quibusdam rerum ipsum alias asperiores! Architecto ullam sequi magnam
+        molestias ab non laboriosam explicabo nisi, facilis error blanditiis,
+        similique culpa provident optio quo. Deleniti, repudiandae quae? Alias
+        debitis esse eveniet consequuntur ex!
+      </p>
+    </div>
+  );
+};
+
+export default Contact;
