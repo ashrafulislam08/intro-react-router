199d2fb3f0e8f3428fd90ee0f14974a8a18aa095
Ashraful Islam <ashrafulislam.dev1@gmail.com>
2024-10-29 22:22:11 +0600

    Final

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,13 +1,13 @@
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 import "./Header.css";
 const Header = () => {
   return (
     <nav>
-      <Link to="/">Home</Link>
-      <Link to="/posts">Posts</Link>
-      <Link to="/users">Users</Link>
-      <Link to="/about">About</Link>
-      <Link to="/contact">Contact</Link>
+      <NavLink to="/">Home</NavLink>
+      <NavLink to="/posts">Posts</NavLink>
+      <NavLink to="/users">Users</NavLink>
+      <NavLink to="/about">About</NavLink>
+      <NavLink to="/contact">Contact</NavLink>
     </nav>
   );
 };
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,13 +1,16 @@
 import React from "react";
 import Header from "../../components/Header/Header";
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigation } from "react-router-dom";
 import Footer from "../../components/Footer/Footer";
 
 const Home = () => {
+  const navigate = useNavigation();
   return (
     <div>
       <Header />
-      <Outlet />
+      <main style={{ height: "100vh" }}>
+        {navigate.state === "loading" ? "Loading" : <Outlet />}
+      </main>
       <Footer />
     </div>
   );
